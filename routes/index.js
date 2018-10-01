1ae11fd1ffecbf517ad1171747b63d62a334924d
Johnrambo <barrars@gmail.com>
2018-10-01 09:22:16 -0700

    ip logging

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  res.render('index', { title: 'Chat Radio' })
+  console.log(req.ip)
 })
 
 module.exports = router
