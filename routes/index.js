405dd3e3adb541f8fde1bdf2fd5d5c22f853a546
Johnrambo <barrars@gmail.com>
2018-10-01 09:48:13 -0700

    ip logging

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Chat Radio' })
 
-  fs.appendFile('ip.log', `${req.ip} connected at ${Date()} `, (err) => {
+  fs.appendFile('ip.log', `${req.ip} connected at ${Date()} \n`, (err) => {
     console.log(err)
   })
   console.log(req.ip)
