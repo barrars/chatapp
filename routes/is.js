de1c3c8e4ab901126ca66d362a1969c7be0836d9
Johnrambo <barrars@gmail.com>
2018-10-04 22:57:55 -0700

    fix chat.  add ip count logging

---
+++ routes/is.js
@@ -0,0 +1,17 @@
+const ipRegex = require('ip-regex')
+const fs = require('fs')
+const path = require('path')
+const _ = require('lodash')
+
+const uniqeVisits = () => {
+  fs.readFile(path.join(__dirname, './../ip.log'), 'utf-8', (err, data) => {
+    console.log(err)
+    console.log(`# of uniqve IP affresses ${_.uniq(data.match(ipRegex())).length}`)
+    let abc =  _.uniq(data.match(ipRegex())).length
+    return abc
+  })
+}
+
+module.exports = {
+  uniqeVisits
+}
