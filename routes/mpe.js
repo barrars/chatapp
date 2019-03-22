28426d6ba262aab20d08486cd6343ff7029c93b6
scott barrar <barrars@gmail.com>
2019-03-21 17:00:20 -0700

    mp3 rename function

---
+++ routes/mpe.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const path = require('path')
+fs.readdir('public/downloads', (err, files) => {
+  if (err) throw err
+  let x = 0
+  files.forEach(file => {
+    if (file.indexOf('.mp3') > 0) {
+      fs.rename('public/downloads/' + file, 'public/downloads/' + file.slice(0, file.indexOf('.mp3')), (err) => {
+        if (err) {
+          console.log(err)
+        }
+        // console.log('done')
+      })
+      // console.log(file.slice(0, file.indexOf('.mp3')))
+    }
+  })
+})
