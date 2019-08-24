82712d0545e5fe0032d1dbcbe09742913dabca29
scott barrar <barrars@gmail.com>
2019-08-23 17:59:57 -0700

    add redirect when :song params not provided

--- routes/playerRouter.js
+++ routes/playerRouter.js
@@ -4,7 +4,15 @@ const express = require('express')
 const router = express.Router()
 router.get('/:song', function (req, res) {
   logger.log(req.params)
-  res.render('playerView', { song: req.params.song })
+  if (req.params.song) {
+    logger.log(req.params.song)
+
+    res.render('playerView', { song: req.params.song })
+  }
+})
+router.get('/', function (req, res) {
+  res.redirect('/')
+  res.render('index', { title: 'Chat Radio', songs: data })
 })
 
 module.exports = router
